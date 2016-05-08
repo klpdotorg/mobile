8b67b2dadda1964c397333b4b05f824c08840da3
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-08 23:46:36 +0530

    Initial commit

---
+++ app/src/androidTest/java/in/org/klp/kontact/ApplicationTest.java
@@ -0,0 +1,13 @@
+package in.org.klp.kontact;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
