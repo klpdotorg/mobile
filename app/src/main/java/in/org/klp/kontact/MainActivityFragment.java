3f2facdfe0db0d01e3c5fd4437bfa458c6dcfd6f
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-09 01:26:04 +0530

    Generates fake data for display

--- app/src/main/java/in/org/klp/kontact/MainActivityFragment.java
+++ app/src/main/java/in/org/klp/kontact/MainActivityFragment.java
@@ -6,6 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +21,18 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] data = {
+                "Akshara Primary School Survey",
+                "GKA Survey",
+                "Akshaya Patra Survey",
+                "PreSchool Survey",
+                "EkStep Survey",
+                "Notorious Survey",
+                "The Ultimate Survey"
+        };
+
+        List<String> Surveys = new ArrayList<String>(Arrays.asList(data));
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
