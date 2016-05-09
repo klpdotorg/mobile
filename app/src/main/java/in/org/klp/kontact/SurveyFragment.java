e9f0af3f883d63450a9d86d03cee75c8f450a9ea
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-09 13:54:33 +0530

    Implements the AsyncTask and puts the API call in it

--- app/src/main/java/in/org/klp/kontact/SurveyFragment.java
+++ app/src/main/java/in/org/klp/kontact/SurveyFragment.java
@@ -1,5 +1,7 @@
 package in.org.klp.kontact;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import org.json.JSONException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -45,11 +49,11 @@ public class SurveyFragment extends Fragment {
         List<String> Surveys = new ArrayList<String>(Arrays.asList(data));
 
         mSurveyAdapter = new ArrayAdapter<String>(
-                        getActivity(),
-                        R.layout.list_item_survey,
-                        R.id.list_item_survey_textview,
-                        Surveys
-                );
+                getActivity(),
+                R.layout.list_item_survey,
+                R.id.list_item_survey_textview,
+                Surveys
+        );
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
@@ -57,5 +61,87 @@ public class SurveyFragment extends Fragment {
         listview.setAdapter(mSurveyAdapter);
 
         return rootView;
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        @Override
+        protected String[] doInBackground(String... params) {
+
+
+            if (params.length == 0) {
+                return null;
+            }
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                final String SURVEY_BASE_URL = "http://dev.klp.org.in/api/v1/surveys/";
+
+                Uri builtUri = Uri.parse(SURVEY_BASE_URL).buildUpon().build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+                Log.v(LOG_TAG, "Forecast JSON string: " + forecastJsonStr);
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+
     }
 }
