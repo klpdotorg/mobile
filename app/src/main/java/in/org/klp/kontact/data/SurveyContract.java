464853dcf7470b48ec0715e01fbe921c20615f17
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-16 17:41:51 +0530

    Adds the Survey table contract data

--- app/src/main/java/in/org/klp/kontact/data/SurveyContract.java
+++ app/src/main/java/in/org/klp/kontact/data/SurveyContract.java
@@ -1,7 +1,18 @@
 package in.org.klp.kontact.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by haris on 5/16/16.
  */
 public class SurveyContract {
+    public static final class SurveyEntry implements BaseColumns {
+        public static final String TABLE_NAME = "survey";
+        public static final String COLUMN_STATUS = "status";
+        public static final String COLUMN_CREATED_AT = "created_at";
+        public static final String COLUMN_START_DATE = "start_date";
+        public static final String COLUMN_END_DATE = "end_date";
+        public static final String COLUMN_PARTNER = "partner";
+        public static final String COLUMN_NAME = "name";
+    }
 }
