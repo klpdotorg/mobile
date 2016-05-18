e73c2e262d10e527c05de655158656d53389ac3e
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-18 20:21:39 +0530

    Launches the SurveyActivity on button click

--- app/src/main/java/in/org/klp/kontact/MainActivity.java
+++ app/src/main/java/in/org/klp/kontact/MainActivity.java
@@ -1,5 +1,6 @@
 package in.org.klp.kontact;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
+                Intent myIntent = new Intent(MainActivity.this, SurveyActivity.class);
+                startActivity(myIntent);
             }
         });
     }
