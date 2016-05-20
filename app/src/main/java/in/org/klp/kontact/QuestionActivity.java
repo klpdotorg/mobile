d05e2506d5b956e74c9aa33bd9ce937ed4adfa47
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-20 16:41:15 +0530

    Adds the Questions screen

---
+++ app/src/main/java/in/org/klp/kontact/QuestionActivity.java
@@ -0,0 +1,17 @@
+package in.org.klp.kontact;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
