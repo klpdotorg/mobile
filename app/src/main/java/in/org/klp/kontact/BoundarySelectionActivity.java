634f5dd319475af0cc7b9ecea923c8036312ea31
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-27 00:00:37 +0530

    Catches the question button click and navigates to the QuestionActivity

--- app/src/main/java/in/org/klp/kontact/BoundarySelectionActivity.java
+++ app/src/main/java/in/org/klp/kontact/BoundarySelectionActivity.java
@@ -1,8 +1,11 @@
 package in.org.klp.kontact;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class BoundarySelectionActivity extends AppCompatActivity {
@@ -18,5 +21,16 @@ public class BoundarySelectionActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.survey_details);
         textView.setText(surveyName);
 
+        Button button = (Button) findViewById(R.id.questions_activity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(BoundarySelectionActivity.this, QuestionActivity.class);
+                intent.putExtra("surveyId", surveyId);
+                intent.putExtra("surveyName", surveyName);
+                startActivity(intent);
+            }
+        });
     }
 }
