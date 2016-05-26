6010def68c5484faf7c51bde6973615bc2951f8c
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-26 23:38:27 +0530

    Overrides the onCreate method to populate the BoundarySelectionActivity

--- app/src/main/java/in/org/klp/kontact/BoundarySelectionActivity.java
+++ app/src/main/java/in/org/klp/kontact/BoundarySelectionActivity.java
@@ -1,5 +1,22 @@
 package in.org.klp.kontact;
 
 
-public class BoundarySelectionActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class BoundarySelectionActivity extends AppCompatActivity {
+    String surveyId;
+    String surveyName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_details);
+        surveyId = getIntent().getStringExtra("surveyId");
+        surveyName = getIntent().getStringExtra("surveyName");
+        TextView textView = (TextView) findViewById(R.id.survey_details);
+        textView.setText(surveyName);
+
+    }
 }
