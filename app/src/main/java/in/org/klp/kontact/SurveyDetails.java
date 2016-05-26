61748153393879d3682124a0be2d2ffefb1a5c96
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-26 23:23:36 +0530

    Catches the new_response button click

--- app/src/main/java/in/org/klp/kontact/SurveyDetails.java
+++ app/src/main/java/in/org/klp/kontact/SurveyDetails.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SurveyDetails extends AppCompatActivity {
@@ -14,7 +16,17 @@ public class SurveyDetails extends AppCompatActivity {
         setContentView(R.layout.activity_survey_details);
         String surveyId = getIntent().getStringExtra("surveyId");
         String surveyName = getIntent().getStringExtra("surveyName");
-        TextView textView=(TextView) findViewById(R.id.survey_details);
+        TextView textView = (TextView) findViewById(R.id.survey_details);
         textView.setText(surveyName);
+
+        Button button = (Button) findViewById(R.id.new_response);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent myIntent = new Intent(SurveyDetails.this, SurveyActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
