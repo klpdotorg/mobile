e91e8a2f1cbe525ab473291ddb5763d9da12fd1a
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-28 00:16:13 +0530

    Adds the BoundaryHierarchyEntry contract

--- app/src/main/java/in/org/klp/kontact/data/SchoolContract.java
+++ app/src/main/java/in/org/klp/kontact/data/SchoolContract.java
@@ -1,5 +1,11 @@
 package in.org.klp.kontact.data;
 
 
+import android.provider.BaseColumns;
+
 public class SchoolContract {
+    public static final class BoundaryHierarchyEntry implements BaseColumns {
+        public static final String TABLE_NAME = "question";
+        public static final String COLUMN_NAME = "name";
+    }
 }
