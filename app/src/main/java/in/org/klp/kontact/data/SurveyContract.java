dbb875e4297c7ee175f47fd4c979d119c137d2fd
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-27 22:58:53 +0530

    Adds Questiongroup Entry Contract

--- app/src/main/java/in/org/klp/kontact/data/SurveyContract.java
+++ app/src/main/java/in/org/klp/kontact/data/SurveyContract.java
@@ -19,4 +19,14 @@ public class SurveyContract {
         public static final String COLUMN_KEY = "key";
         public static final String COLUMN_SCHOOL_TYPE = "school_type_id";
     }
+
+    public static final class QuestiongroupEntry implements BaseColumns {
+        public static final String TABLE_NAME = "questiongroup";
+        public static final String COLUMN_STATUS = "status";
+        public static final String COLUMN_START_DATE = "start_date";
+        public static final String COLUMN_END_DATE = "end_date";
+        public static final String COLUMN_VERSION = "version";
+        public static final String COLUMN_SOURCE = "source";
+        public static final String COLUMN_SURVEY = "survey_id";
+    }
 }
