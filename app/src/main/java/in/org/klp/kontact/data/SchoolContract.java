736d5186d86b8122986073075179e767ff4f9b2c
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-28 22:52:15 +0530

    Implements the Boundary Entry Contract

--- app/src/main/java/in/org/klp/kontact/data/SchoolContract.java
+++ app/src/main/java/in/org/klp/kontact/data/SchoolContract.java
@@ -5,7 +5,17 @@ import android.provider.BaseColumns;
 
 public class SchoolContract {
     public static final class BoundaryHierarchyEntry implements BaseColumns {
-        public static final String TABLE_NAME = "question";
+        public static final String TABLE_NAME = "boundary_hierarchy";
         public static final String COLUMN_NAME = "name";
     }
+
+    public static final class BoundaryEntry implements BaseColumns {
+        public static final String TABLE_NAME = "boundary";
+        public static final String COLUMN_PARENT = "boundary_id";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_HIERARCHY = "hierarchy";
+        public static final String COLUMN_TYPE = "type";
+    }
+
+    
 }
