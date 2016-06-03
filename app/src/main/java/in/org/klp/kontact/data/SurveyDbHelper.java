5d4d4d0d62b4519f507f9511536a098772e430a9
Bibhas <iambibhas@gmail.com>
2016-06-03 10:49:02 +0530

    fixed constructor

--- app/src/main/java/in/org/klp/kontact/data/SurveyDbHelper.java
+++ app/src/main/java/in/org/klp/kontact/data/SurveyDbHelper.java
@@ -18,11 +18,14 @@ import in.org.klp.kontact.data.SchoolContract.SchoolEntry;
 
 public class SurveyDbHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
-
     static final String DATABASE_NAME = "kontact.db";
 
     public SurveyDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
                           int version) {
+        super(context, name, factory, version);
+    }
+
+    public SurveyDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
