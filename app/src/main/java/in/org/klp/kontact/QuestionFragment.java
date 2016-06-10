fc4da10bc21e6edaac58dfea87c56f3d53c96d24
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-06-10 21:55:16 +0530

    Adds the FetchQuestionsTask method

--- app/src/main/java/in/org/klp/kontact/QuestionFragment.java
+++ app/src/main/java/in/org/klp/kontact/QuestionFragment.java
@@ -1,6 +1,8 @@
 package in.org.klp.kontact;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,14 +12,30 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
+
+import in.org.klp.kontact.data.SurveyDbHelper;
 
 public class QuestionFragment extends Fragment {
 
     private ArrayAdapter<String> mQuestionsAdapter;
-
+    String surveyId;
+    String surveyName;
 
     public QuestionFragment() {
+    }
+
+    private void fetchQuestions() {
+        FetchQuestionsTask questionsTask = new FetchQuestionsTask();
+        questionsTask.execute();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        fetchQuestions();
     }
 
     @Override
@@ -31,11 +49,42 @@ public class QuestionFragment extends Fragment {
                 new ArrayList<String>()
         );
 
+        Intent intent = getActivity().getIntent();
+
+        surveyId = intent.getStringExtra("surveyId");
+        surveyName = intent.getStringExtra("surveyName");
+
         View rootView = inflater.inflate(R.layout.fragment_question, container, false);
 
         ListView listview = (ListView) rootView.findViewById(R.id.listview_question);
         listview.setAdapter(mQuestionsAdapter);
 
         return rootView;
+    }
+
+    public class FetchQuestionsTask extends AsyncTask<Void, Void, String[]> {
+
+        private final String LOG_TAG = FetchQuestionsTask.class.getSimpleName();
+        SurveyDbHelper dbHelper;
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String surveyJsonStr = null;
+
+            dbHelper = new SurveyDbHelper(getActivity());
+
+            Cursor cursor = dbHelper.list_surveys();
+            if (cursor.getCount() >= 1) {
+                String surveyId;
+            }
+            return null;
+        }
     }
 }
