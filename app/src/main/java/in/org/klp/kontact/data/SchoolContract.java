c089c7375a07133b1de2942a5dfc775cd8e83796
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-06-12 16:16:42 +0530

    Removes the BoundaryHierarchy contract

--- app/src/main/java/in/org/klp/kontact/data/SchoolContract.java
+++ app/src/main/java/in/org/klp/kontact/data/SchoolContract.java
@@ -1,14 +1,8 @@
 package in.org.klp.kontact.data;
 
-
 import android.provider.BaseColumns;
 
 public class SchoolContract {
-    public static final class BoundaryHierarchyEntry implements BaseColumns {
-        public static final String TABLE_NAME = "boundary_hierarchy";
-        public static final String COLUMN_NAME = "name";
-    }
-
     public static final class BoundaryEntry implements BaseColumns {
         public static final String TABLE_NAME = "boundary";
         public static final String COLUMN_PARENT = "boundary_id";
