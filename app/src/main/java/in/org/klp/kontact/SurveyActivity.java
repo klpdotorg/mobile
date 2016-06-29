bcdc2e4298879c266742d70f1e2672263bf41a9c
Bibhas <iambibhas@gmail.com>
2016-06-29 09:16:16 +0530

    changed colors and some UI fixes

--- app/src/main/java/in/org/klp/kontact/SurveyActivity.java
+++ app/src/main/java/in/org/klp/kontact/SurveyActivity.java
@@ -12,8 +12,8 @@ public class SurveyActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
     }
 
     @Override
--- app/src/main/java/in/org/klp/kontact/adapters/QuestionAdapter.java
+++ app/src/main/java/in/org/klp/kontact/adapters/QuestionAdapter.java
@@ -52,6 +52,10 @@ public class QuestionAdapter extends ArrayAdapter<Question> {
         return answers;
     }
 
+    public void addAnswer(Question question, String answer) {
+        this.answers.put(question, answer);
+    }
+
     @Override
     public View getView(int position, View convertView, final ViewGroup parent) {
         final Question question = getItem(position);
