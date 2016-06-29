379a809779cc3960f990baae651fec7d974a5b00
Bibhas <iambibhas@gmail.com>
2016-06-29 09:25:53 +0530

    hiding boundary selection section when scrolling to make space for smaller screens

--- app/src/main/java/in/org/klp/kontact/BoundarySelectionActivity.java
+++ app/src/main/java/in/org/klp/kontact/BoundarySelectionActivity.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
